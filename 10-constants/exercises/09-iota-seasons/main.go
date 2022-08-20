10ce32061ef54070b1a047f7e8bbdee950c56789
goelvatsal <goel.vatsal@outlook.com>
2022-08-20 14:38:47 -0400

    completed iota exercises

--- 10-constants/exercises/09-iota-seasons/main.go
+++ 10-constants/exercises/09-iota-seasons/main.go
@@ -1,11 +1,3 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
 import "fmt"
@@ -26,10 +18,10 @@ func main() {
 	// NOTE : You should remove all the initializers below
 	//        first. Then use iota to fix it.
 	const (
-		Winter = 12
-		Spring = 3
-		Summer = 6
-		Fall   = 9
+		Winter = 12 - (iota * 3)
+		Fall
+		Summer
+		Spring
 	)
 
 	fmt.Println(Winter, Spring, Summer, Fall)
