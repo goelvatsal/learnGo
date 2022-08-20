aca7d5012b21bf9b1d083e686c9ba3d01e691315
goelvatsal <goel.vatsal@outlook.com>
2022-08-20 14:30:39 -0400

    completed raw string literal exercises

--- 08-numbers-and-strings/02-strings/exercises/02-print-json/main.go
+++ 08-numbers-and-strings/02-strings/exercises/02-print-json/main.go
@@ -1,11 +1,3 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
 import "fmt"
@@ -31,14 +23,14 @@ func main() {
 	// \n equals to newline character
 	// \" equals to double-quotes character
 
-	json := "\n" +
-		"{\n" +
-		"\t\"Items\": [{\n" +
-		"\t\t\"Item\": {\n" +
-		"\t\t\t\"name\": \"Teddy Bear\"\n" +
-		"\t\t}\n" +
-		"\t}]\n" +
-		"}\n"
+	json := `
+{
+	"Items": [{
+		"Item": {
+			"name": "Teddy Bear"
+		}
+	}]
+}`
 
 	fmt.Println(json)
 }
--- 08-numbers-and-strings/02-strings/exercises/08-right-trim-it/main.go
+++ 08-numbers-and-strings/02-strings/exercises/08-right-trim-it/main.go
@@ -1,15 +1,9 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode/utf8"
 )
 
 // ---------------------------------------------------------
@@ -33,5 +27,5 @@ func main() {
 	// it should print 5
 
 	name := "inanç           "
-	fmt.Println(len(name))
+	fmt.Println(utf8.RuneCountInString(strings.TrimRight(name, " ")))
 }
