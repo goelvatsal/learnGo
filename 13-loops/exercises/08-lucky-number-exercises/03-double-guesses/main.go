4b66f74a299b5297562d15716714d52d5dd14d65
goelvatsal <goel.vatsal@outlook.com>
2022-08-23 15:03:24 -0400

    completed some rand exercises

--- 13-loops/exercises/08-lucky-number-exercises/03-double-guesses/main.go
+++ 13-loops/exercises/08-lucky-number-exercises/03-double-guesses/main.go
@@ -8,19 +8,82 @@
 
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Double Guesses
 //
-//  Let the player guess two numbers instead of one.
+//  Let the player guess1 two numbers instead of one.
 //
 // HINT:
 //  Generate random numbers using the greatest number
-//  among the guessed numbers.
+//  among the guess1ed numbers.
 //
 // EXAMPLES
 //  go run main.go 5 6
 //  Player wins if the random number is either 5 or 6.
 // ---------------------------------------------------------
 
+const (
+	maxTurns = 5 // less is more difficult
+	usage    = `Welcome to the Lucky Number Game! 🍀
+
+The program will pick %d random numbers.
+Your mission is to guess1 one of those numbers.
+
+The greater your number is, harder it gets.
+
+Wanna play?
+`
+)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	args := os.Args[1:]
+
+	if len(args) != 2 {
+		fmt.Printf(usage, maxTurns)
+		return
+	}
+
+	guess1, err1 := strconv.Atoi(args[0])
+
+	if err1 != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	guess2, err2 := strconv.Atoi(args[1])
+	if err2 != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	if guess1 < 0 || guess2 < 0 {
+		fmt.Println("Please pick a positive number.")
+		return
+	}
+
+	min := guess1
+	if guess1 < guess2 {
+		min = guess2
+	}
+
+	for turn := 0; turn < maxTurns; turn++ {
+		n := rand.Intn(min + 1)
+
+		if n == guess1 || n == guess2 {
+			fmt.Println("🎉  YOU WIN!")
+			return
+		}
+	}
+
+	fmt.Println("☠️  YOU LOST... Try again?")
 }
