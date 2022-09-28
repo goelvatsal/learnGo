4cfd93755a796c608d767d911c11294da1e38f96
goelvatsal <goel.vatsal@outlook.com>
2022-09-27 23:09:44 -0400

    fixed slices1 changes

--- 16-slices/exercises/03-slice-literal/main.go
+++ 16-slices/exercises/03-slice-literal/main.go
@@ -55,13 +55,14 @@ func main() {
 	data = []byte{8, 2, 7, 0, 5}
 	ratios = []float64{1.01, 141.76}
 	alives = []bool{true, true, true, false}
-	var s string
 
 	fmt.Printf("names    : %T %d %t\n", names, len(names), names == nil)
 	fmt.Printf("distances: %T %d %t\n", distances, len(distances), distances == nil)
 	fmt.Printf("data     : %T %d %t\n", data, len(data), data == nil)
 	fmt.Printf("ratios   : %T %d %t\n", ratios, len(ratios), ratios == nil)
 	fmt.Printf("alives   : %T %d %t\n", alives, len(alives), alives == nil)
+
+	var s string
 	if len(distances) != len(data) {
 		s = "not "
 	}
